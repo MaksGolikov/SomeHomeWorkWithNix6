c763cb91ea6d07bc731aea1a8a3d879845b0337c
MaksGolikov <maksim.golikov01@gmail.com>
2020-06-28 15:40:28 +0300

    some 1 principle SRP

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,20 @@
+import io.Data;
 import service.AddService;
 
+import java.util.Date;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a: ");
-        int firstNumber = scanner.nextInt();
-        System.out.print("Enter b: ");
-        int secondNumber = scanner.nextInt();
-
+        Data data = new Data(scanner);
         AddService addService = new AddService();
+
+        int firstNumber = data.input("Enter a: ");
+        int secondNumber = data.input("Enter b: ");
         int sum = addService.add(firstNumber, secondNumber);
 
-        System.out.print("Result: ");
-        System.out.println(sum);
+        data.print("Result: ");
+        data.print(sum);
     }
 }
